c809f3d572cf138e0ca5904151c7bfcf0241b3de
Prayag Sangode <prayag.rhce@gmail.com>
2022-07-08 21:15:05 +0530

    add dir

---
+++ install/aws-cli-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Author : Prayag Sangode
+#
+On your other Linux system install aws cli
+
+# mkdir aws-install
+
+# cd aws-install
+
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+# unzip awscliv2.zip
+
+# sudo ./aws/install
+
+# ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+
+# which aws
+
+# aws --version
+
+Configure AWS and provide access key and secret key when prompted -
+
+# aws configure
